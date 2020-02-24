5f620ab9f689b3cb3509e54c07acb21c60c29f50
KameriiJ <jenny.sirarat@gmail.com>
2020-02-24 19:57:37 +0700

    first commit

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public Light[] lights;
+    public GameObject particle;
+    public bool isOn = true;
+
+    void Awake()
+    {
+        lights = FindObjectsOfType<Light>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Light light in lights)
+        {
+            light.enabled = isOn;
+            particle.SetActive(isOn);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        foreach(Light light in lights)
+        {
+            light.enabled = !light.enabled;
+            particle.SetActive(light.enabled);
+        }
+    }
+
+    //check that another GO is enter to the area: no bounce
+    void OnTriggerEnter(Collider other)
+    {
+        print("trigger: " + other.gameObject.name);
+    }
+
+    //check that another GO is collish this GO: bounce
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collision: " + collision.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float jumpPow = 10f;
+    Vector3[] v3 = { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(v3[Random.Range(0, 4)] * jumpPow, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
+        }
+        if (transform.position.y < -50)
+        {
+            transform.position = new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Input.GetAxis("Horizontal")*15f, 0, Input.GetAxis("Vertical")*15f);
+    }
+}
