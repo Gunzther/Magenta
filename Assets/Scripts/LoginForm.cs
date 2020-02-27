6834722de0977278baf2b6923875364933406abd
KameriiJ <jenny.sirarat@gmail.com>
2020-02-27 13:37:02 +0700

    added menu scene

---
+++ Assets/Scripts/LoginForm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginForm : MonoBehaviour
+{
+    public InputField userField;
+    public InputField passwordField;
+    
+    public void CheckUserAndPassword()
+    {
+        if(userField.text == passwordField.text)
+        {
+            print("pass");
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            print("please try again");
+        }
+    }
+}
---
+++ Assets/Scripts/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class RegisterModel { 
+    public string UserName;
+    public string Password;
+    public string gender;
+    public int age;
+    public bool accepted;
+}
