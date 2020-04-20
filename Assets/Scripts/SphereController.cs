ec64c7f21abc81ea5cfc64d47677bec004669eda
KameriiJ <jenny.sirarat@gmail.com>
2020-04-20 22:05:28 +0700

    map floor 1: doors, toom tui, light path 1

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -17,10 +17,10 @@ public class SphereController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
-        if (Input.GetKey(KeyCode.RightArrow)) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
-        if (Input.GetKey(KeyCode.UpArrow)) transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
-        if (Input.GetKey(KeyCode.DownArrow)) transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        if (Input.GetKey(KeyCode.A)) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        if (Input.GetKey(KeyCode.D)) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        if (Input.GetKey(KeyCode.W)) transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        if (Input.GetKey(KeyCode.S)) transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
         if (transform.position.y <= -5) transform.position = new Vector3(0, 2f, 0.1f);
     }
 }
